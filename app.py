57129054f584f1050cdd9cb3d02e5183009597f0
karanotts <nottinghamkara@gmail.com>
2021-04-12 10:16:12 +0100

    add database context

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
 
 app = Flask(__name__)
+
+# db context
+def connect_db():
+    sql = sqlite3.connect('food_log.db')
+    sql.row_factory = sqlite3.Row
+    return sql
+
+def get_db():
+    if not hasattr(g, 'sqlite3_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite3
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
 
 @app.route('/')
 def index():
---
+++ food_tracker.sql
@@ -0,0 +1,21 @@
+-- sqlite3 food_log.db < food_tracker.sql 
+
+create table log_date (
+    id integer primary key autoincrement,
+    entry_date date not null
+);
+
+create table food (
+    id integer primary key autoincrement,
+    name text not null,
+    protein integer not null,
+    carbohydrates integer not null,
+    fats integer not null,
+    calories integer not null
+);
+
+create table food_date (
+    food_id integer not null,
+    log_date_id integer not null,
+    primary key(food_id, log_date_id)
+);
