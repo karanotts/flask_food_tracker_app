2e7524dbfb2e7795e40fb7eb3940f792bd47c583
karanotts <nottinghamkara@gmail.com>
2021-04-12 11:28:24 +0100

    add food item from form to db

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request
 import sqlite3
 
 app = Flask(__name__)
@@ -10,9 +10,9 @@ def connect_db():
     return sql
 
 def get_db():
-    if not hasattr(g, 'sqlite3_db'):
+    if not hasattr(g, 'sqlite_db'):
         g.sqlite_db = connect_db()
-    return g.sqlite3
+    return g.sqlite_db
 
 @app.teardown_appcontext
 def close_db(error):
@@ -30,9 +30,27 @@ def view():
     return render_template('day.html')
 
 
-@app.route('/food')
+@app.route('/food', methods=['GET', 'POST'])
 def food():
-    return render_template('add_food.html')
+    db = get_db()
+
+    if request.method == 'POST':
+        name = request.form['food-name']
+        protein = int(request.form['protein'])
+        carbohydrates = int(request.form['carbohydrates'])
+        fat = int(request.form['fat'])
+        calories = protein * 4 + carbohydrates * 4 + fat * 9
+
+        db.execute(
+            'insert into food (name, protein, carbohydrates, fat, calories) values (?, ?, ?, ?, ?)', \
+            [name, protein, carbohydrates, fat, calories]
+            )
+        db.commit()
+
+    cursor = db.execute('select name, protein, carbohydrates, fat, calories from food')
+    results = cursor.fetchall()
+
+    return render_template('add_food.html', results=results)
 
 
 
